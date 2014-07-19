d1fea80b7b93534835b752984083743abf6c4f3b
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2014-07-19 16:47:52 +0900

    tol as an argument; conversion to strings if not np.array

--- mc_compute_stationary_mpmath.py
+++ mc_compute_stationary_mpmath.py
@@ -17,7 +17,7 @@ except ImportError:
     sys.exit(1)
 
 
-def mc_compute_stationary_mpmath(P, dps=17):
+def mc_compute_stationary_mpmath(P, precision=None, tol=1e-17):
     """
     Computes the stationary distributions of Markov matrix P.
 
@@ -26,14 +26,17 @@ def mc_compute_stationary_mpmath(P, dps=17):
     P : array_like(float, ndim=2)
         A discrete Markov transition matrix
 
-    dps : scalar(int), optional
-        Decimal precision
-        Find eigenvectors for eigenvalues > 1 - 1e-(dps)
+    precision : scalar(int), optional
+        Decimal precision in float-point arithemetic with mpmath
+
+    tol: scalar(float), optional
+        Tolerance level
+        Find eigenvectors for eigenvalues in [1-tol, 1+tol]
 
     Returns
     -------
     vecs : list of numpy.arrays of sympy.mpmath.ctx_mp_python.mpf
-        A list of the eigenvectors of whose eigenvalues > 1 - 1e-(dps)
+        A list of the eigenvectors of whose eigenvalues > 1 - 1e-(precision)
 
     References
     ----------
@@ -42,20 +45,28 @@ def mc_compute_stationary_mpmath(P, dps=17):
         http://mpmath.org/doc/current/basics.html#setting-the-precision
 
     """
-    tmp = mp.mp.dps  # Store the current decimal precision
-    mp.mp.dps = dps  # Set decimal precision to dps
+    if precision:
+        tmp = mp.mp.dps  # Store the current decimal precision
+        mp.mp.dps = precision  # Set decimal precision to precision
 
-    TOL = '1e-{0}'.format(dps)  # Tolerance
+    TOL = str(tol)
 
-    E, EL = mp.eig(mp.matrix(P), left=True, right=False)
+    if not isinstance(P, np.ndarray):
+        Q = np.empty([len(P), len(P)], dtype='|S{0}'.format(mp.mp.dps))
+        for i, row in enumerate(P):
+            Q[i] = ['{0:.{1}f}'.format(x, mp.mp.dps) for x in row]
+        E, EL = mp.eig(mp.matrix(Q), left=True, right=False)
+    else:
+        E, EL = mp.eig(mp.matrix(P), left=True, right=False)
 
     vecs = []
 
     for i, val in enumerate(E):
-        if val > mp.mpf(1) - mp.mpf(TOL):
+        if mp.mpf(1) - mp.mpf(TOL) <= val <= mp.mpf(1) + mp.mpf(TOL):
             vec = np.array(EL[i, :].tolist()[0])
             vecs.append(vec/sum(vec))
 
-    mp.mp.dps = tmp  # Restore the current decimal precision
+    if precision:
+        mp.mp.dps = tmp  # Restore the current decimal precision
 
     return vecs
